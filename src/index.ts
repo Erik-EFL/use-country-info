a9b1ee78a1df9cd5b72aaf8a172448a29ec7b4a1
Erik-EFL <erik.lima.f@outlook.com>
2023-12-14 17:28:50 -0300

    fix: :art: Remove code to improve reading
    Removed unnecessary logic to another location

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,21 @@
-/* eslint-disable no-unsafe-optional-chaining */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable import/no-unresolved */
-import { useState } from 'react';
-import apiData from './constant/data';
+import { useMemo } from 'react';
+import { apiData } from './constant/data';
 import { TCountryFlags } from './types/contry-flags.type';
 import { TCountryInfo } from './types/contry.type';
 import { TDdiInfo } from './types/ddi.type';
+import { mapCountryFlags, mapDdiInfo } from './utils/map.utils';
 
 export const useCountryInfo = () => {
-  const [allCountryInfo, setAllCountryInfo] = useState<TCountryInfo[]>(
-    (): TCountryInfo[] => apiData
+  const allCountryInfo = useMemo<TCountryInfo[]>(() => apiData, []);
+
+  const ddiInfo = useMemo<TDdiInfo[]>(
+    () => apiData.map(mapDdiInfo),
+    [mapDdiInfo]
   );
 
-  const [ddiInfo, setDDInfo] = useState<TDdiInfo[]>((): TDdiInfo[] =>
-    apiData.map((data) => {
-      const { countryCallingCode, flag, flags } = data;
-      return { countryCallingCode, flag, flags };
-    })
-  );
-
-  const [countryflags, setCountryflags] = useState<TCountryFlags[]>(
-    (): TCountryFlags[] =>
-      apiData.map((data) => {
-        const { flag, flags, name } = data;
-
-        return { flag, flags, name };
-      })
+  const countryflags = useMemo<TCountryFlags[]>(
+    () => apiData.map(mapCountryFlags),
+    [mapCountryFlags]
   );
 
   return {
